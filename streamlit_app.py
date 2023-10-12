13e7561ca812d493f170b4ed54bb58d4c6afc438
Mateusz Wilczyński <matwilq@gmail.com>
2023-10-12 16:01:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.write('The user entered ', fruits_selected[-1])
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruits_selected[-1])
