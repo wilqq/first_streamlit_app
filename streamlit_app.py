649e60bbb105e5032deaa6dc130782ba3f3d2174
Mateusz Wilczyński <m.wilczynski@exlabs.pl>
2023-10-16 16:46:03 +0200

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,13 +43,24 @@ try:
 except URLError as e:
   streamlit.error("Error: Connection refused")
 
-# streamlit.stop()
 
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT * from fruit_load_list")
-# my_data_rows = my_cur.fetchall()
-# streamlit.header("The fruit load list contains:")
-# streamlit.dataframe(my_data_rows)
+streamlit.header("The fruit load list contains:")
 
-# streamlit.header("Fruityvice Fruit Advice!")
+def get_fruit_load_list():
+  with my_cnx.coursor() as my_cur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    my_data_rows = my_cur.fetchall()
+    return my_data_rows
+
+if streamlit.button("Get Fruit Load List"):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+
+streamlit.stop()
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
