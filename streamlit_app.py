7eae71c896de91432bfbcdb44fe92a2e66b29e81
Mateusz Wilczyński <matwilq@gmail.com>
2023-10-16 15:55:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 streamlit.text('after table')
+
+import snowflake.connector
