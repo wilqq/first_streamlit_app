3a4c73e0e37a991813537593b5ab41cbdbbb7ba2
Mateusz Wilczyński <m.wilczynski@exlabs.pl>
2023-10-16 16:40:16 +0200

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,14 +27,19 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+
+def get_fruitvice_data(this_fruit_choice):
+  fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruitvice_response.json())
+  return fruityvice_normalized
+
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information')
   else:
-    fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruitvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    function_response = get_fruitvice_data(fruit_choice)
+    streamlit.dataframe(function_response)
 except URLError as e:
   streamlit.error("Error: Connection refused")
 
