606c630aad3cb8da48254c4910f58f4cee12b9fe
Mateusz Wilczyński <matwilq@gmail.com>
2023-10-16 16:26:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,19 +27,20 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
- 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-streamlit.text('after table')
+try:
+ fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ if not fruit_choice:
+   streamlit.error('Please select a fruit to get information')
+ else:
+  fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
 
 
 
 streamlit.stop()
+
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
@@ -49,5 +50,11 @@ streamlit.dataframe(my_data_rows)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice_2 = streamlit.text_input('Input some text','Avocado')
-streamlit.write('The user entered ', fruit_choice_2)
+try:
+ fruit_choice_2 = streamlit.text_input('What fruit would you like information about?')
+ if not fruit_choice_2:
+   streamlit.error('Please select a fruit to get information')
+ else:
+  fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+ streamlit.write('The user entered ', fruit_choice_2)
